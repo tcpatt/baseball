857efde9b8757a1fc332dab5d4671f298a476079
Taylor Patterson <tcpatt@gmail.com>
2015-11-07 09:00:13 -0500

    Adding copyright statements.

--- tmn.java.project/src/tmn/java/project/Game.java
+++ tmn.java.project/src/tmn/java/project/Game.java
@@ -1,6 +1,13 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2015 Taylor Patterson
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Taylor Patterson - Initial implementation and documentation
+ *******************************************************************************/
 package tmn.java.project;
 
 import java.net.URL;
