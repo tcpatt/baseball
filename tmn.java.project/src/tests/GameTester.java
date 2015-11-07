d6ef8852039992915b73246c0b64b1705a299507
Taylor Patterson <tcpatt@gmail.com>
2015-11-07 09:10:08 -0500

    Adding Eclipse project data and unimplemented Player and Game tests.

---
+++ tmn.java.project/src/tests/GameTester.java
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Taylor Patterson
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Taylor Patterson - Initial implementation and documentation
+ *******************************************************************************/
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tmn.java.project.Game;
+
+/**
+ * This class defines the JUnit tests for {@link Game} objects.
+ * 
+ * @author Taylor Patterson
+ */
+public class GameTester {
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getRbi()}.
+	 */
+	@Test
+	public void testGetRbi() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#setRbi(int)}.
+	 */
+	@Test
+	public void testSetRbi() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getAtBats()}.
+	 */
+	@Test
+	public void testGetAtBats() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#setAtBats(int)}.
+	 */
+	@Test
+	public void testSetAtBats() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getHits()}.
+	 */
+	@Test
+	public void testGetHits() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#setHits(int)}.
+	 */
+	@Test
+	public void testSetHits() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getOpponentImg()}.
+	 */
+	@Test
+	public void testGetOpponentImg() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link tmn.java.project.Game#setOpponentImg(java.net.URL)}.
+	 */
+	@Test
+	public void testSetOpponentImg() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getOpponentId()}.
+	 */
+	@Test
+	public void testGetOpponentId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link tmn.java.project.Game#setOpponentId(java.lang.String)}.
+	 */
+	@Test
+	public void testSetOpponentId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getHomeRuns()}.
+	 */
+	@Test
+	public void testGetHomeRuns() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#setHomeRuns(int)}.
+	 */
+	@Test
+	public void testSetHomeRuns() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getPlayerTeamId()}.
+	 */
+	@Test
+	public void testGetPlayerTeamId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link tmn.java.project.Game#setPlayerTeamId(java.lang.String)}.
+	 */
+	@Test
+	public void testSetPlayerTeamId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getPlayerTeamImg()}.
+	 */
+	@Test
+	public void testGetPlayerTeamImg() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link tmn.java.project.Game#setPlayerTeamImg(java.net.URL)}.
+	 */
+	@Test
+	public void testSetPlayerTeamImg() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#getGameDate()}.
+	 */
+	@Test
+	public void testGetGameDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Game#setGameDate(java.util.Date)}
+	 * .
+	 */
+	@Test
+	public void testSetGameDate() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tmn.java.project/src/tests/PlayerTester.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Taylor Patterson
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Taylor Patterson - Initial implementation and documentation
+ *******************************************************************************/
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tmn.java.project.Player;
+
+/**
+ * This class defines the JUnit tests for {@link Player} objects.
+ * 
+ * @author Taylor Patterson
+ */
+public class PlayerTester {
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#Player()}.
+	 */
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#Player(java.io.File)}.
+	 */
+	@Test
+	public void testPlayerFile() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#getId()}.
+	 */
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#setId(java.lang.Integer)}.
+	 */
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#getGames()}.
+	 */
+	@Test
+	public void testGetGames() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#setGames(java.util.List)}.
+	 */
+	@Test
+	public void testSetGames() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#setName(java.lang.String)}
+	 * .
+	 */
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#getImagePath()}.
+	 */
+	@Test
+	public void testGetImagePath() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link tmn.java.project.Player#setImagePath(java.net.URL)}.
+	 */
+	@Test
+	public void testSetImagePath() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link tmn.java.project.Player#writeToHTML(java.net.URL)}
+	 * .
+	 */
+	@Test
+	public void testWriteToHTML() {
+		fail("Not yet implemented");
+	}
+
+}
