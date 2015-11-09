e4e3fbd11f0dc4ff8108894f769dd400a9485cc3
Taylor Patterson <tcpatt@gmail.com>
2015-11-08 20:39:33 -0500

    Implemented the file selection dialog.

--- tmn.java.project/src/tmn/java/project/Controller.java
+++ tmn.java.project/src/tmn/java/project/Controller.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 /**
  * This class contains the main method for this project and acts as a control
@@ -24,27 +25,57 @@ import javax.swing.JFileChooser;
  */
 public class Controller {
 
-	/*
-	 * The main method for this project.
+	/**
+	 * This method presents the user with a dialog to select files with which to
+	 * create {@link Player} objects and HTML files.
 	 */
-	public static void main(String[] args) {
-
-		// Present a dialog to the user to select player data files to use
-		final JFileChooser fileChooser = new JFileChooser();
-
-		// Allow for multiple file selection and present the dialog to the user
+	private static void presentFileChooser() {
+		// Construct a dialog for the user to select player data files to use
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Select player data files");
 		fileChooser.setMultiSelectionEnabled(true);
-		fileChooser.setVisible(true);
 
-		// Collect the user's file selection
-		List<File> files = Arrays.asList(fileChooser.getSelectedFiles());
+		// Present the dialog to the user
+		int openFileReturn = fileChooser.showOpenDialog(null);
 
-		// Loop over the list of selected files, create a Player objects for
-		// each file, and generate an HTML file for each Player
+		// Continue if the user selected one or more files
+		if (JFileChooser.APPROVE_OPTION == openFileReturn) {
+			// Collect the user's file selection and attempt to create HTML
+			// files for the player data
+			List<File> files = Arrays.asList(fileChooser.getSelectedFiles());
+			createHTMLFiles(files);
+		} else {
+			// No files selected
+			// Notify the user that the application will terminate
+			JOptionPane.showMessageDialog(null,
+					"No files were selected. The application will now terminate.",
+					"No selection", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	/**
+	 * This method loops over the list of input files and attempts to create
+	 * {@link Player} objects for the input and then write HTML files for the
+	 * player data.
+	 * 
+	 * @param files
+	 *            The list of files to use to try to create {@link Player}
+	 *            objects and HTML files
+	 */
+	private static void createHTMLFiles(List<File> files) {
 		for (File file : files) {
 			Player player = new Player(file);
 			player.writeToHTML("path/to/working/directory/html/filename.html");
 		}
+	}
+
+	/*
+	 * The main method for this project.
+	 */
+	public static void main(String[] args) {
+		// Present a file selection dialog to the user.
+		// Control is handled elsewhere from here.
+		presentFileChooser();
 	}
 
 }
