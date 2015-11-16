8fb5fd7128826194a7442f65b9406209538195e9
Taylor Patterson <tcpatt@gmail.com>
2015-11-16 11:47:20 -0500

    Finishing work on Game test class.
    The tests for Player and Game are not a full test suite. Rather, this is
    just a sample and employs some frowned upon conventions.

--- tmn.dev.project/src/tests/GameTester.java
+++ tmn.dev.project/src/tests/GameTester.java
@@ -38,152 +38,108 @@ public class GameTester {
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#getRbi()}.
+	 * Test method for {@link Game#setRbi(int)} and {@link Game#getRbi()}.
 	 */
 	@Test
-	public void testGetRbi() {
-		fail("Not yet implemented");
+	public void testSetAndGetRbi() {
+		assert(-1 == testGame.getRbi());
+		int testRbi = 1;
+		testGame.setRbi(testRbi);
+		assert(testRbi == testGame.getRbi());
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#setRbi(int)}.
+	 * Test method for {@link Game#setAtBats(int)} and {@link Game#getAtBats()}.
 	 */
 	@Test
-	public void testSetRbi() {
-		fail("Not yet implemented");
+	public void testSetAndGetAtBats() {
+		assert(-1 == testGame.getAtBats());
+		int testAb = 2;
+		testGame.setAtBats(testAb);
+		assert(testAb == testGame.getAtBats());
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#getAtBats()}.
+	 * Test method for {@link Game#setHits(int)} and {@link Game#getHits()}.
 	 */
 	@Test
-	public void testGetAtBats() {
-		fail("Not yet implemented");
+	public void testSetAndGetHits() {
+		assert(-1 == testGame.getHits());
+		int testH = 3;
+		testGame.setHits(testH);
+		assert(testH == testGame.getHits());
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#setAtBats(int)}.
+	 * Test method for {@link Game#setOpponentImg(String)} and
+	 * {@link Game#getOpponentImg()}.
 	 */
 	@Test
-	public void testSetAtBats() {
-		fail("Not yet implemented");
+	public void testSetAndGetOpponentImg() {
+		assertNull(testGame.getOpponentImg());
+		String testPath = "https://fake.url/det.png";
+		testGame.setOpponentImg(testPath);
+		assertEquals(testPath, testGame.getOpponentImg());
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#getHits()}.
+	 * Test method for {@link Game#setOpponentId(String)} and
+	 * {@link Game#getOpponentId()}.
 	 */
 	@Test
-	public void testGetHits() {
-		fail("Not yet implemented");
+	public void testSetAndGetOpponentId() {
+		assertNull(testGame.getOpponentId());
+		String testId = "DET";
+		testGame.setOpponentId(testId);
+		assertEquals(testId, testGame.getOpponentId());
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#setHits(int)}.
+	 * Test method for {@link Game#setHomeRuns(int)} and
+	 * {@link Game#getHomeRuns()}.
 	 */
 	@Test
-	public void testSetHits() {
-		fail("Not yet implemented");
+	public void testSetAndGetHomeRuns() {
+		assert(-1 == testGame.getHomeRuns());
+		int testHr = 4;
+		testGame.setHomeRuns(testHr);
+		assert(testHr == testGame.getHomeRuns());
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#getOpponentImg()}.
+	 * Test method for {@link Game#setPlayerTeamId(String)} and
+	 * {@link Game#getPlayerTeamId()}.
 	 */
 	@Test
-	public void testGetOpponentImg() {
-		fail("Not yet implemented");
+	public void testSetAndGetPlayerTeamId() {
+		assertNull(testGame.getPlayerTeamId());
+		String testId = "CLE";
+		testGame.setPlayerTeamId(testId);
+		assertEquals(testId, testGame.getPlayerTeamId());
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#setOpponentImg(java.net.URL)}
-	 * .
+	 * Test method for {@link Game#setPlayerTeamImg(String)} and
+	 * {@link Game#getPlayerTeamImg()}.
 	 */
 	@Test
-	public void testSetOpponentImg() {
-		fail("Not yet implemented");
+	public void testSetAndGetPlayerTeamImg() {
+		assertNull(testGame.getPlayerTeamImg());
+		String testPath = "https://fake.url/cle.png";
+		testGame.setPlayerTeamImg(testPath);
+		assertEquals(testPath, testGame.getPlayerTeamImg());
 	}
 
 	/**
-	 * Test method for {@link tmn.dev.project.Game#getOpponentId()}.
+	 * Test method for {@link Game#setGameDate(String)} and
+	 * {@link Game#getGameDate()}.
 	 */
 	@Test
-	public void testGetOpponentId() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link tmn.dev.project.Game#setOpponentId(java.lang.String)}.
-	 */
-	@Test
-	public void testSetOpponentId() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link tmn.dev.project.Game#getHomeRuns()}.
-	 */
-	@Test
-	public void testGetHomeRuns() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link tmn.dev.project.Game#setHomeRuns(int)}.
-	 */
-	@Test
-	public void testSetHomeRuns() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link tmn.dev.project.Game#getPlayerTeamId()}.
-	 */
-	@Test
-	public void testGetPlayerTeamId() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link tmn.dev.project.Game#setPlayerTeamId(java.lang.String)}.
-	 */
-	@Test
-	public void testSetPlayerTeamId() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link tmn.dev.project.Game#getPlayerTeamImg()}.
-	 */
-	@Test
-	public void testGetPlayerTeamImg() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link tmn.dev.project.Game#setPlayerTeamImg(java.net.URL)}.
-	 */
-	@Test
-	public void testSetPlayerTeamImg() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link tmn.dev.project.Game#getGameDate()}.
-	 */
-	@Test
-	public void testGetGameDate() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link tmn.dev.project.Game#setGameDate(java.util.Date)}
-	 * .
-	 */
-	@Test
-	public void testSetGameDate() {
-		fail("Not yet implemented");
+	public void testSetAndGetGameDate() {
+		assertNull(testGame.getGameDate());
+		String testDate = "08-02-1989";
+		testGame.setGameDate(testDate);
+		assertEquals(testDate, testGame.getGameDate());
 	}
 
 }
