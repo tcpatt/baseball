2aa870befac7ac7caee06c109d77eeb80ec1e80a
Taylor Patterson <tcpatt@gmail.com>
2015-11-20 12:28:39 -0500

    Adding a little navigation and form entry work.

--- tmn.qa.project/src/tmn/qa/project/TestDriver.java
+++ tmn.qa.project/src/tmn/qa/project/TestDriver.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * 
@@ -42,11 +43,36 @@ public class TestDriver {
 		// Click the link to the desired repository
 		driver.findElement(By.linkText("tcpatt/tmn")).click();
 
+		// Simple search for TruMedia
+		element = driver.findElement(By.name("q"));
+		element.sendKeys("TruMedia");
+		element.submit();
+
+		// Advanced search for TruMedia
+		driver.findElement(By.linkText("advanced search")).click();
+		element = driver.findElement(By.id("search_repos"));
+		element.sendKeys("tcpatt/tmn");
+		Select dropdown = new Select(
+				driver.findElement(By.id("search_language")));
+		dropdown.selectByValue("Java");
+		element.submit();
+
 		// Navigate to the test file
-		driver.findElement(By.id("d4728e20f195aa3992a6172487f5f91f-abf49617197"
-				+ "196df17708d1c9fe79bbed69862ec")).click();
+		// driver.findElement(By.id("d4728e20f195aa3992a6172487f5f91f-abf49617197"
+		// + "196df17708d1c9fe79bbed69862ec")).click();
 		// element = driver.findElement(By.className("file"));
 		// System.out.println(element.getText());
+
+		driver.get("http://www.trumedianetworks.com");
+		driver.findElement(By.id("email")).click();
+		element = driver.findElement(By.name("fname"));
+		element.sendKeys("Taylor");
+		element = driver.findElement(By.name("lname"));
+		element.sendKeys("Patterson");
+		element = driver.findElement(By.id("email2-field"));
+		element.sendKeys("tcpatt@gmail.com");
+		element = driver.findElement(By.id("textarea1-field"));
+		element.sendKeys("Test message");
 
 		// Close the browser
 		driver.quit();
